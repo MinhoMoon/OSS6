52cbbef3540d2f3e35caf2cd6e4e815f047af92b
jaehun306 <63222161+jaehun306@users.noreply.github.com>
2020-06-15 10:35:57 +0900

    LOGIN

---
+++ app/src/main/java/com/project/oss6/ResultActivity.java
@@ -0,0 +1,34 @@
+package com.project.oss6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;//닉네임 TEXT
+    private ImageView iv_profile; // 이미지 프로필
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("nickName"); // MainActivity로 부터 닉네임 받음
+        String photoUrl = intent.getStringExtra("photoUrl"); // MainActivity로 부터 프로필사진 url 받음
+
+        tv_result = findViewById(R.id.tv_result);
+        tv_result.setText(nickName); // 닉네임 text를 텍스트뷰에 세팅
+
+        iv_profile = findViewById(R.id.iv_profile);
+        Glide.with(this).load(photoUrl).into(iv_profile); // 프로필 url을 이미지 뷰에 세팅
+
+    }
+}
